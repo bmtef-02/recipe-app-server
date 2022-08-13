26e282ad0b603d885f488c71a3a18920c1193eb3
Brian Jun <bmtef.02@gmail.com>
2022-08-13 00:05:07 -0700

    added Procfile, tried to deploy on Heroku

--- app.js
+++ app.js
@@ -31,6 +31,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/recipes', recipesRouter);
+app.get('/', (req, res) => { res.send('Hello from Express!')});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
