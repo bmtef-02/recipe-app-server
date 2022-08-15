14de679c5ec296307e0195d614afc6aad0c12538
Brian Jun <bmtef.02@gmail.com>
2022-08-14 18:37:40 -0700

    remove testing if server deployed on Heroku

--- app.js
+++ app.js
@@ -29,8 +29,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use('/', indexRouter);
-app.use('/', (req, res) => { res.send('Hello from Express!')});
+app.use('/', indexRouter);
 app.use('/recipes', recipesRouter);
 
 // catch 404 and forward to error handler
