f85ce6dc58a4be638792c24d127cbee3f2d4e7a9
Brian Jun <bmtef.02@gmail.com>
2022-08-15 00:16:28 -0700

    CORS added to recipes route

--- routes/recipes.js
+++ routes/recipes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const Recipe = require('../models/recipe')
 const recipesRouter = express.Router();
+const cors = require('./cors');
 
 recipesRouter.route('/')
-.get((req, res, next) => {
+.options(cors.corsWithOptions, (req, res) => res.sendStatus(200))
+.get(cors.corsWithOptions, (req, res, next) => {
     Recipe.find()
     .then(recipes => {
         res.statusCode = 200;
@@ -12,7 +14,7 @@ recipesRouter.route('/')
     })
     .catch(err => next(err));
 })
-.post((req, res) => {
+.post(cors.corsWithOptions, (req, res) => {
     Recipe.create(req.body)
     .then(recipe => {
         console.log(`Recipe Created: ${recipe}`);
@@ -22,11 +24,11 @@ recipesRouter.route('/')
     })
     .catch(err => next(err));
 })
-.put((req, res) => {
+.put(cors.corsWithOptions, (req, res) => {
     res.statusCode = 403;
     res.end('PUT operation not supported on /recipes');
 })
-.delete((req, res, next) => {
+.delete(cors.corsWithOptions, (req, res, next) => {
     Recipe.deleteMany()
     .then(response => {
         res.statusCode = 200;
@@ -37,7 +39,8 @@ recipesRouter.route('/')
 })
 
 recipesRouter.route('/:recipeId')
-.get((req, res, next) => {
+.options(cors.corsWithOptions, (req, res) => res.sendStatus(200))
+.get(cors.corsWithOptions, (req, res, next) => {
     Recipe.findById(req.params.recipeId)
     .then(recipe => {
         console.log(`Found recipe: ${recipe}`);
@@ -47,11 +50,11 @@ recipesRouter.route('/:recipeId')
     })
     .catch(err => next(err));
 })
-.post((req, res) => {
+.post(cors.corsWithOptions, (req, res) => {
     res.statusCode = 403;
     res.end('POST operation is not available for this route');
 })
-.put((req, res, next) => {
+.put(cors.corsWithOptions, (req, res, next) => {
     Recipe.findByIdAndUpdate(req.params.recipeId, { $set: req.body }, { new: true })
     .then(recipe => {
         console.log(`Updated recipe: ${recipe}`);
@@ -61,7 +64,7 @@ recipesRouter.route('/:recipeId')
     })
     .catch(err => next(err));
 })
-.delete((req, res, next) => {
+.delete(cors.corsWithOptions, (req, res, next) => {
     Recipe.findByIdAndDelete(req.params.recipeId)
     .then(recipe => {
         console.log(`Recipe deleted: ${recipe}`);
